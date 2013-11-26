5a4fa59e607c623f3c64262d34947ebae3cb7c1d
Nicola Peduzzi <thenikso@gmail.com>
2013-11-26 16:25:52 +0100

    Add `MouseHit` script to stop movement when hitting an enemy

---
+++ Assets/Scripts/MouseHit.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MouseHit : MonoBehaviour {
+	public static Transform Target { get; private set; }
+
+	private Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// When pressing mouse down and no GUI control has been hit
+		if (Input.GetMouseButton(0) && GUIUtility.hotControl == 0) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if( Physics.Raycast( ray, out hit, 100 ) && hit.transform == myTransform ) {
+				MouseHit.Target = myTransform;
+			} else if (MouseHit.Target == myTransform) {
+				MouseHit.Target = null;
+			}
+		} else {
+			MouseHit.Target = null;
+		}
+	}
+}
--- Assets/Scripts/Player/MoveOnMouseClick.cs
+++ Assets/Scripts/Player/MoveOnMouseClick.cs
@@ -1,24 +1,4 @@
-﻿/* 
- * Esse Script movimenta o GameObject quando você clica ou
- * mantém o botão esquerdo do mouse apertado.
- * 
- * Para usá-lo, adicione esse script ao gameObject que você quer mover
- * seja o Player ou outro
- * 
- * Autor: Vinicius Rezendrix - Brasil
- * Data: 11/08/2012
- * 
- * This script moves the GameObeject when you
- * click or click and hold the LeftMouseButton
- * 
- * Simply attach it to the gameObject you wanna move (player or not)
- * 
- * Autor: Vinicius Rezendrix - Brazil
- * Data: 11/08/2012 
- *
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 
 public class MoveOnMouseClick : MonoBehaviour {
 	private Transform myTransform;				// this transform
@@ -26,9 +6,7 @@ public class MoveOnMouseClick : MonoBehaviour {
 	private float destinationDistance;			// The distance between myTransform and destinationPosition
 	
 	public float moveSpeed;						// The Speed the character will move
-	
-	
-	
+
 	void Start () {
 		myTransform = transform;							// sets myTransform to this GameObject.transform
 		destinationPosition = myTransform.position;			// prevents myTransform reset
@@ -46,24 +24,8 @@ public class MoveOnMouseClick : MonoBehaviour {
 			moveSpeed = 3;
 		}
 		
-		
-		// Moves the Player if the Left Mouse Button was clicked
-		if (Input.GetMouseButtonDown(0)&& GUIUtility.hotControl ==0) {
-			
-			Plane playerPlane = new Plane(Vector3.up, myTransform.position);
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			float hitdist = 0.0f;
-			
-			if (playerPlane.Raycast(ray, out hitdist)) {
-				Vector3 targetPoint = ray.GetPoint(hitdist);
-				destinationPosition = ray.GetPoint(hitdist);
-				Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
-				myTransform.rotation = targetRotation;
-			}
-		}
-		
 		// Moves the player if the mouse button is hold down
-		else if (Input.GetMouseButton(0)&& GUIUtility.hotControl ==0) {
+		if (Input.GetMouseButton(0)&& GUIUtility.hotControl ==0) {
 			
 			Plane playerPlane = new Plane(Vector3.up, myTransform.position);
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -71,11 +33,14 @@ public class MoveOnMouseClick : MonoBehaviour {
 			
 			if (playerPlane.Raycast(ray, out hitdist)) {
 				Vector3 targetPoint = ray.GetPoint(hitdist);
-				destinationPosition = ray.GetPoint(hitdist);
+				if (MouseHit.Target != null) {
+					destinationPosition = myTransform.position;
+				} else {
+					destinationPosition = targetPoint;
+				}
 				Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
 				myTransform.rotation = targetRotation;
 			}
-			//	myTransform.position = Vector3.MoveTowards(myTransform.position, destinationPosition, moveSpeed * Time.deltaTime);
 		}
 		
 		// To prevent code from running if not needed
