2d937fa2d2e133adaeece8d3f67c415dfac3c8c1
Uri Baghin <skerfat@gmail.com>
2013-11-26 12:32:55 +0100

    Added player character and movement.

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	public float movementSpeed;
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 movement = Vector3.zero;
+		if (Input.GetKey (KeyCode.W)) {
+			movement += Vector3.forward;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			movement += Vector3.left;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			movement += Vector3.back;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			movement += Vector3.right;
+		}
+
+		movement.Normalize ();
+		movement *= movementSpeed;
+
+		transform.Translate (movement);
+	}
+}
